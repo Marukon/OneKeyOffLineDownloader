5df954496900f44dee1d10174c6b1358702c6657
techoyk <techoyk@gmail.com>
2019-12-30 00:02:18 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # OneKeyOffLineDownloader
 Onekey install OffLineDownloader
+wget -N --no-check-certificate https://raw.githubusercontent.com/MRAUStuty/OneKeyOffLineDownloader/master/install.sh && chmod +x install.sh && bash install.sh
--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#=================================================
+#	System Required: CentOS/Debian/Ubuntu
+#	Description: OneKeyOffLineDownloader
+#	Version: 1.0
+#	Author: Mr.Au
+#	Github: https://github.com/MRAUStuty/OneKeyOffLineDownloader
+#=================================================
 wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.13.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
